bcfb336d469872221b567ee37bdf4da9f0a241bc
Jason Stathopulos <jstathopulos@gmail.com>
2022-08-26 20:02:22 -0700

    Add Order Table Component
    Add the Order Table component to display the orders to the user

---
+++ frontend/src/OrderTable/OrderTable.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react'
+import styled from 'styled-components'
+
+import { Order } from '../types'
+
+const NoOrdersFoundWrapper = styled.h2`
+  margin: 80px;
+  textalign: center;
+`
+
+export type OrderTableProps = {
+  orders: Order[]
+}
+
+export const OrderTable: FC<OrderTableProps> = ({ orders }) => {
+  if (orders.length === 0) {
+    return <NoOrdersFoundWrapper>No Orders Found</NoOrdersFoundWrapper>
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th scope="col">Customer</th>
+          <th scope="col">Price</th>
+          <th scope="col">Item</th>
+          <th scope="col">Destination</th>
+        </tr>
+      </thead>
+      <tbody>
+        {orders.map((order: Order) => (
+          <tr key={order.id}>
+            <th scope="row">{order.customer}</th>
+            <td>${order.price / 100}</td>
+            <td>{order.item}</td>
+            <td>{order.destination}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
---
+++ frontend/src/OrderTable/__tests__/OrderTable.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react'
+import { EventName } from '../../types'
+
+import { OrderTable, OrderTableProps } from '../OrderTable'
+
+describe('OrderTable', () => {
+  const defaultProps: OrderTableProps = {
+    orders: [],
+  }
+  const setUp = (props: Partial<OrderTableProps> = {}) => {
+    render(<OrderTable {...defaultProps} {...props} />)
+  }
+
+  it('should show table', async () => {
+    setUp({
+      orders: [
+        {
+          id: 'dfsajkllf423',
+          event_name: EventName.CREATED,
+          price: 1024,
+          item: 'Pizza',
+          customer: 'John Smith',
+          destination: '123 Test Ave',
+          sent_at_second: 36,
+        },
+      ],
+    })
+
+    await screen.findByText('John Smith')
+    await screen.findByText('123 Test Ave')
+    await screen.findByText('$10.24')
+  })
+
+  describe('when no orders are found', () => {
+    it('should show not found message', async () => {
+      setUp()
+
+      await screen.findByText('No Orders Found', { exact: false })
+    })
+  })
+})
