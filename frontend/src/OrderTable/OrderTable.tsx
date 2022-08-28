9d52524ec593cad98c221e46103378830051c67f
Jason Stathopulos <jstathopulos@gmail.com>
2022-08-28 12:06:55 -0700

    Add Styling to Order Table Component
    Add the styling to the Order Table component as it takes up most of the
    screen, so it should look presentable to the user so that they can see
    all the orders coming in.

--- frontend/src/OrderTable/OrderTable.tsx
+++ frontend/src/OrderTable/OrderTable.tsx
@@ -9,6 +9,45 @@ const NoOrdersFoundWrapper = styled.h2`
   textalign: center;
 `
 
+const OrdersTable = styled.table`
+  width: 100%;
+  display: table;
+  border-spacing: 0;
+  border-collapse: collapse;
+
+  tr {
+    color: inherit;
+    display: table-row;
+    outline: 0;
+    vertical-align: middle;
+  }
+
+  th {
+    font-weight: 500;
+    padding-bottom: 16px;
+    text-align: left;
+  }
+`
+
+const OrdersTableHeader = styled.thead`
+  width: 100%;
+  border-spacing: 0;
+  border-collapse: collapse;
+`
+
+const OrdersTableBody = styled.tbody`
+  display: table-row-group;
+
+  td {
+    display: table-cell;
+    padding: 16px 0;
+    text-align: left;
+    font-weight: 400;
+    border-bottom: 1px solid rgba(81, 81, 81, 1);
+    width: 25%;
+  }
+`
+
 export type OrderTableProps = {
   orders: Order[]
 }
@@ -19,25 +58,25 @@ export const OrderTable: FC<OrderTableProps> = ({ orders }) => {
   }
 
   return (
-    <table>
-      <thead>
+    <OrdersTable>
+      <OrdersTableHeader>
         <tr>
           <th scope="col">Customer</th>
           <th scope="col">Price</th>
           <th scope="col">Item</th>
           <th scope="col">Destination</th>
         </tr>
-      </thead>
-      <tbody>
+      </OrdersTableHeader>
+      <OrdersTableBody>
         {orders.map((order: Order) => (
           <tr key={order.id}>
-            <th scope="row">{order.customer}</th>
+            <td>{order.customer}</td>
             <PriceCell price={order.price} />
             <td>{order.item}</td>
             <td>{order.destination}</td>
           </tr>
         ))}
-      </tbody>
-    </table>
+      </OrdersTableBody>
+    </OrdersTable>
   )
 }
