63f5c5f894741080e3704ca3721e4f44bef3bf5f
Jason Stathopulos <jstathopulos@gmail.com>
2022-08-28 17:25:03 -0700

    Add Test Utils For Components Use State Management
    Add the Test Utils for components that use the state management so that
    they can set up their test to be in a state that the developer to run
    the exact state multiple times.

---
+++ frontend/src/testUtil/AppProvider.tsx
@@ -0,0 +1,32 @@
+import { FC, ReactNode } from 'react'
+import { AppContext } from '../contexts'
+import { Action, InitialState } from '../types'
+
+export const initialState: InitialState = {
+  orders: {},
+  searchText: '',
+}
+
+export interface AppProviderProps {
+  state: InitialState
+  children: ReactNode
+  dispatch?: (action: Action) => void
+}
+
+export const AppProvider: FC<AppProviderProps> = ({
+  state,
+  dispatch,
+  children,
+}) => (
+  <AppContext.Provider
+    value={{
+      state: {
+        ...initialState,
+        ...state,
+      },
+      dispatch: dispatch || (() => null),
+    }}
+  >
+    {children}
+  </AppContext.Provider>
+)
